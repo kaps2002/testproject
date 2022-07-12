7964712f92faeb783e0e7d93f74f45db03a41443
Karan Pandey <karpandey2002@gmail.com>
2022-07-12 15:06:33 +0530

    String.js and object.js

---
+++ object.js
@@ -0,0 +1,18 @@
+const car ={
+    color:'green',
+    gears:5,
+    engine:"diesel"
+
+}
+// const somefunc=({color='red',engine:carengine=6}={})=>{
+// console.log(color,carengine);
+// }
+// somefunc({});
+// const {color,gears,engine:carengine}=car;
+// console.log(`color ${car.color}`);
+// console.log(`gears ${car.gears}`);
+// console.log(`carengine ${car.engine}`);
+
+const somarr=[1,2,3,4,5]
+var [secondel]=somarr;
+console.log(secondel);
--- string.js
+++ string.js
@@ -1,7 +1,7 @@
-const name='karn';
-const age=10;
-const getmyname =()=> "john";
-const result ="My name is"   + name + age;
-const result2 = `my name is ${getmyname()}`;
-console.log(result);
-console.log(result2);
+// const name='karn';
+// const age=10;
+// const getmyname =()=> "john";
+// const result ="My name is"   + name + age;
+// const result2 = `my name is ${getmyname()}`;
+// console.log(result);
+// console.log(result2);
